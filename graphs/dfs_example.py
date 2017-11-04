7f532042ffb819963a7639e9bbe7125aa7db1b2b
Patrick Olden <patrickolden@googlemail.com>
2017-11-04 13:36:11 +0000

    dfs

--- graphs/dfs_example.py
+++ graphs/dfs_example.py
@@ -1,3 +1,5 @@
+# http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+
 from collections import defaultdict
 
 graph = defaultdict(set)
@@ -8,17 +10,46 @@ graph['B'].add('D')
 graph['C'].add('A')
 
 
-# Find all paths between two nodes
-connecting_paths = []
+# Imperative Connected Components
+def dfs_imperative(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex])
+    return visited
 
 
-def fap(graph, curr, end, path=[]):
-    path.append(curr)
-    if curr == end:
-        connecting_paths.append(path[:])
-    for node in (n for n in graph[curr] if n not in path):
-        fap(graph, node, end, path)
-    del path[-1]
+def dfs_recursive(graph, curr, visited=set()):
+    if curr not in visited:
+        visited.add(curr)
+        for _next in graph[curr]:
+            dfs_recursive(graph, _next, visited)
+    return visited
 
-fap(graph, 'A', 'C')
-print(connecting_paths)
+
+#  DO THIS WITH BFS IT'D USE LESS SPACE
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    paths = []
+    while stack:
+        (vertex, path) = stack.pop()
+        for _next in graph[vertex] - set(path):
+            if _next == goal:
+                paths.append(path + [_next])
+            else:
+                stack.append((_next, path + [_next]))
+    return paths
+
+
+def dfs_recursive(graph, curr, visited=set()):
+    if curr not in visited:
+        visited.add(curr)
+        for n in graph[curr]:
+            dfs_recursive(graph, n, visited)
+    return visited
+
+
+
+dfs_recursive(graph, 'A')
