00573035b3c6144afb1ae5d11adf0359beb6a4ca
Patrick Olden <patrickolden@googlemail.com>
2018-01-03 11:52:11 +0000

    spiralorder2

--- java/SpiralOrder2.java
+++ java/SpiralOrder2.java
@@ -12,8 +12,41 @@
 //   [ 7, 6, 5 ]
 // ]
 
+import java.util.ArrayList;
+
 class SpiralOrder2 {
-	public static void main(string args[]) {
-		System.out.printl('Trial')
+	public static void main(String args[]) {
+
+		ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
+
+		Integer n = 4;
+
+		for (Integer i=0; i<n; i++) {
+			ArrayList<Integer> row = new ArrayList<Integer>();
+			for (Integer j=0; j<n; j++){
+				row.add(0);
+			}
+			grid.add(row);
+		}
+
+		Integer i=0, j=0, di=0, dj=1, tempi=0;
+
+		for (Integer k=1; k<=n*n; k++) {
+			grid.get(i).set(j, k);
+			if (grid.get(Math.abs((i + di) % n)).get(Math.abs((j + dj) % n)) != 0) {
+				tempi = di; 
+				di = dj;
+				dj = -tempi;
+			}
+			i += di;
+			j += dj;
+		}
+
+		for (i=0; i<n; i++) {
+			for (j=0; j<n; j++){
+				System.out.format("i=%d, j=%d, vak=%d \n", i, j, grid.get(i).get(j));
+			}
+		}
+
 	}
 }
