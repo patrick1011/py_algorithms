2803cba936c0f9a192c2f5a46b7fae359c27bd7e
Patrick Olden <patrickolden@googlemail.com>
2018-01-05 11:44:52 +0000

    begin strategy pattern

---
+++ java/design_patterns/strategy.java
@@ -0,0 +1,14 @@
+// Strategy Pattern
+
+// The strategy pattern:
+//		1. Defines a family of algorithms.
+//		2. Encapsulates each one (makes them interchangable).
+// Strategy makes the algorithms vary independently of the clients who use it.
+
+// e.g. list -> inject a sorting algorithm into a list.
+
+class Strategy {
+	public static void main(String args[]) {
+		System.out.println("yohoo");
+	}	
+}
